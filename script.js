14700786f958d15cb3e8d38a1d23bc40d7c957c2
jhanavigupta <jhanavigupta3936@gmail.com>
2020-07-08 04:32:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// let num1 = prompt('please enter number 1');
+// let operations = prompt('add the operation');
+// let num2 = prompt('please enter number 2');
+
+
+
+// console.log(num1, num2, operations);
+
+// switch (operations) {
+
+//     case `+`:
+//         console.log(`sum is ${parseInt(num1) + parseInt(num2)}`);
+//         break;
+
+//     case `-`:
+//         console.log(`result is ${num1 - num2}`);
+//         break;
+
+//     case `/`:
+//         console.log(`result is ${num1 / num2}`);
+//         break;
+
+//     case `*`:
+//         console.log(`result is ${num1 * num2}`);
+//         break;
+
+//     case `**`:
+//         console.log(`result is ${num1 ** num2}`);
+//         break;
+
+//     case `%`:
+//         console.log(`remainder is ${num1 % num2}`);
+//         break;
+
+//     default:
+//         console.log(`invalid operation`);
+//         break;
+// }
